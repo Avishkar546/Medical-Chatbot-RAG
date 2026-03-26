e81f9f23fc0920cea105496e546960620e822a54
Avishkar <gaikwadavishkar546@gmail.com>
2026-03-26 12:12:13 +0530

    Initial setup

---
+++ template.sh
@@ -0,0 +1,16 @@
+# Creating directories
+mkdir -p src
+mkdir -p research
+
+# creating files
+touch .env
+touch app.py
+touch setup.py
+touch requirements.txt
+touch src/__init__.py
+touch src/helpers.py
+touch src/prompt.py
+touch research/experiment.ipynb
+touch research/experiment.ipynb
+
+echo "Directories and files are created successfully !!"
